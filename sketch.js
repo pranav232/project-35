b700ee03c896681f225546da8154608259686ca3
pranav232 <73473120+pranav232@users.noreply.github.com>
2021-05-10 16:09:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+var balloon,balloonImage1,balloonImage2;
+var database
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+  var ht =database.ref('balloon/height')
+  ht.on("value",readheight,showeror)
+
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    updateheight(-10,0)
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    updateheight(10,0)
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    updateheight(0,-10)
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    updateheight(0,10)
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function readheight(data){
+  pos=data.val();
+  balloon.x=pos.x;
+  balloon.y=pos.y;
+
+}
+function showeror(){
+  console.log("eror")
+}
+function updateheight(x,y){
+database.ref('balloon/height').set({
+  x:pos.x+x,y:pos.y+y
+})
+}
